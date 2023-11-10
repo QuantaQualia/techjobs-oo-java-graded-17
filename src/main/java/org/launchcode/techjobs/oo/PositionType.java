f24ad9da98cb0eb224073c93ba26aa187f3f703a
QuantaQualia <markatkins3000@outlook.com>
2023-11-10 00:56:04 -0600

    Fixed dumb mistakes like putting whole blocks of code in the wrong damn file.

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -29,6 +29,19 @@ public class PositionType {
     //  their id fields match.
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
+
     // Getters and Setters:
 
     public int getId() {
@@ -43,16 +56,4 @@ public class PositionType {
         this.value = value;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PositionType that = (PositionType) o;
-        return id == that.id && Objects.equals(value, that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, value);
-    }
 }
--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,12 @@
 package org.launchcode.techjobs.oo;
 
+import org.testng.annotations.Test;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId(){
+
+    }
 }
