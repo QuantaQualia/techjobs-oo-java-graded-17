686cde850384ca419395bc3b0b8db30ecff578ab
QuantaQualia <markatkins3000@outlook.com>
2023-11-09 19:07:31 -0600

    First commit

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -4,9 +4,31 @@ import java.util.Objects;
 
 public class CoreCompetency {
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     private int id;
     private static int nextId = 1;
     private String value;
+
+    private String name;
+    private String employer;
+    private String location;
+    private String positionType;
+    private String coreCompetency;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 
     public CoreCompetency() {
         this.id = nextId;
@@ -17,6 +39,21 @@ public class CoreCompetency {
         this();
         this.value = value;
     }
+
+    public CoreCompetency(int id) {
+        this();
+        this.id = id;
+    }
+
+    public CoreCompetency (String name, String employer, String location, String positionType, String coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
 
     // Custom toString, equals, and hashCode methods:
 
@@ -38,7 +75,51 @@ public class CoreCompetency {
         return Objects.hash(id);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(String employer) {
+        this.employer = employer;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(String positionType) {
+        this.positionType = positionType;
+    }
+
+    public String getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(String coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
+
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
 
+
+    public static int getNextId() {
+        return nextId;
+    }
 }
--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -18,8 +20,14 @@ public class PositionType {
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
+    public String toString(String value){
+        this.value = value;
+        return value;
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
 
     // Getters and Setters:
 
@@ -35,4 +43,16 @@ public class PositionType {
         this.value = value;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
 }
